9b59f7dde3616deacbbab167f4f23158d6ba6c3f
Melissa Knapp <melknapp@gmail.com>
2019-10-21 22:18:59 -0400

    Total score increased by the value of the crystal every time one is clicked and displayed on the html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,16 @@ console.log("numToMatch is " + numToMatch);
 $("#number-to-match").html(numToMatch);
 
 var crystalNums = [];
+var totalScore = 0;
 
+//assign random values between 1 and 12 to each of the crystals
 for (var i = 0; i < 4; i++) {
     crystalNums[i] = Math.floor(Math.random() * 11 + 1);
     console.log("crystalNums[" + i + "] is " + crystalNums[i]);
 }
 
 
-console.log("crystalNums[0] is " + crystalNums[0]);
+//send the images of the crystals along with their value to the html
 
 $("#crystal1").html("<img class='crystal' src='assets/images/crystal1.jpg' value = " + crystalNums[0] + " />");
 $("#crystal2").html("<img class='crystal' src='assets/images/crystal2.jpg' value = " + crystalNums[1] + " />");
@@ -25,7 +27,17 @@ console.log(crystalImg);
 crystalImg.append("Is this working?");
 
 crystalImg.on("click", function() {
-    alert("The click function is active");
+    var crystalValue = ($(this).attr("value"));
+    crystalValue = parseInt(crystalValue);
+    console.log("crystalValue is " + crystalValue);
+
+    //adds the value of the crystal clicked to the total score
+    totalScore += crystalValue;
+
+    //sends the total score to the html
+    $("#total-score").html(totalScore);
+    
+
 
 })
 
