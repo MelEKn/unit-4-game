8f4538eddf95f8df03ec19f1d348f4b521312bc5
Melissa Knapp <melknapp@gmail.com>
2019-10-21 21:22:26 -0400

    created random numbers for each crystal and put them in an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,13 @@
+//crystal images taken from https://www.rd.com/beauty/healing-crystals/ 
+
 var numToMatch = Math.floor(Math.random() * 101 + 19);
 console.log("numToMatch is " + numToMatch);
 
 $("#number-to-match").html(numToMatch);
+
+var crystalNums = [];
+
+for(var i=0; i<4; i++){
+    crystalNums[i] = Math.floor(Math.random() * 11 + 1);
+    console.log("crystalNums[" + i + "] is " + crystalNums[i]);
+}
